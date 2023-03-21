0c9f603f921642b1a31f30070da871e711dfbdab
brijesh <brijeshdubeyid@gmail.com>
2023-03-21 16:34:59 +0530

    Added filter

--- src/app/features/user-settings/components/betting-pl/betting-pl.component.ts
+++ src/app/features/user-settings/components/betting-pl/betting-pl.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettingsMainService } from '../../services/user-settings-main.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-betting-pl',
@@ -9,37 +10,37 @@ import { UserSettingsMainService } from '../../services/user-settings-main.servi
 export class BettingPlComponent implements OnInit {
   profitLoss : any[] = [];
   options:any = {
-    // autoApply?: boolean;
-    // clickOutsideAllowed?: boolean;
+    autoApply:false,
+    clickOutsideAllowed:false,
     // displayFormat?: string;
-    // disabled?: boolean;
-    // disableInputDisplay?: boolean;
+    disabled:true,
+    // disableInputDisplay:true;
     format: 'DD MMM YYYY',
     // icons?: 'default' | 'material' | 'font-awesome';
-    // labelText?: string;
     // minDate: new Date().toJSON().slice(0,10).replace(/-/g,'/')
     // maxDate:;
     // position?: 'left' | 'right';
     // preDefinedRanges?: IDefinedDateRange[];
     // showResetButton?: boolean;
-    singleCalendar : true
+    // singleCalendar : true
     // validators?: ValidatorFn | ValidatorFn[];
     // modal?: boolean;
   };
+  fromDate = moment().format("YYYY-MM-DD");
+  toDate = moment().format("YYYY-MM-DD");
 
   constructor(
-    private _userSettingsService: UserSettingsMainService,
-    // private _userSettingsService:UserSettingsDataService
+    private _userSettingsService: UserSettingsMainService
   ) { }
 
   ngOnInit(): void {
-    this.getProfitLoss();
+    this.getProfitLoss(this.fromDate,this.toDate);
   }
 
-  getProfitLoss(){
+  getProfitLoss(fromDate,toDate){
     let profitLossObj = {
-      fromDate:"2023-02-19",
-      toDate:"2023-03-21",
+      fromDate:moment(fromDate).format("YYYY-MM-DD"),
+      toDate:moment(toDate).format("YYYY-MM-DD"),
       game: null
     };
     this._userSettingsService._getProfitLossApi(profitLossObj).subscribe(
@@ -50,10 +51,19 @@ export class BettingPlComponent implements OnInit {
     );
   }
 
+  rangeSelected(event){
+    this.fromDate = event.start._d
+    this.toDate = event.end._d
+  }
+
   setPlBet(bets){
     console.log(bets)
     this._userSettingsService.setPlBets(bets)
     console.log('res',bets)
+  }
+
+  getProfitAndLoss(){
+    this.getProfitLoss(this.fromDate,this.toDate);
   }
 
 }
