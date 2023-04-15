7631aaaeca258da938c651015068914c319ac6c4
Jasvinder Singh <jasvinder.rajput@cylsys.com>
2023-04-15 16:51:41 +0400

    settings issue

--- src/app/features/user-settings/components/settings/settings.component.ts
+++ src/app/features/user-settings/components/settings/settings.component.ts
@@ -18,6 +18,7 @@ export class SettingsComponent implements OnInit {
   editBettingEditStake:FormArray;
   isEditMode = false;
   isEditMode1 = false;
+  isLoading = false;
   constructor(
     private _userSettingsService: UserSettingsMainService,
     private _formBuilder: FormBuilder,
@@ -25,20 +26,22 @@ export class SettingsComponent implements OnInit {
     private _location: Location,
 
     ) { }
-    
+
     onActivateAll(){
       this.oneClickBetting=!this.oneClickBetting;
     }
-    
-  
-  ngOnInit(): void {
-    this.getUserConfig();
+
+
+  ngOnInit(): void {console.log(this.isLoading,'this.isLoading start');
+    //this.getUserConfig();
+    this.isLoading = true;
     this._userSettingsService._getUserConfigApi().subscribe(
       (res:any) => {
         if(res?.userConfig){
           this.userConfig = res;
-          this.editBetting=new FormArray(res['userConfig']['editOneClickStakeBtn'].map(_singleValue=>new FormControl(_singleValue)))
-          this.editBettingEditStake=new FormArray(res['userConfig']['EditStakesBtn'].map(_singleValue1=>new FormControl(_singleValue1)))
+          this.editBetting=new FormArray(res['userConfig']['editOneClickStakeBtn'].map(_singleValue=>new FormControl(_singleValue)));
+          this.editBettingEditStake=new FormArray(res['userConfig']['EditStakesBtn'].map(_singleValue1=>new FormControl(_singleValue1)));
+          this.isLoading = false;
         }
       });
   }
@@ -46,7 +49,7 @@ export class SettingsComponent implements OnInit {
   getControl(index){
     return this.editBetting.at(index) as FormControl
   }
-  
+
   getControl1(index){
     return this.editBettingEditStake.at(index) as FormControl
   }
@@ -70,12 +73,12 @@ export class SettingsComponent implements OnInit {
     }
     this._userSettingsService._getSaveUserConfigApi(saveUser).subscribe(
       (res) => {
-        this.userConfig = res  ;      
+        this.userConfig = res  ;
       });
       this._sharedService.getToastPopup("User Settings saved sucessfully",'Settings','success');
 
     }
-    
+
     goBack(){
       this._location.back();
     }
