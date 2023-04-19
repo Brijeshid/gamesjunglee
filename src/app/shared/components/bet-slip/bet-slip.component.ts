4ab07ec268f0033216e1a91d644d74ff29f78c7a
RoopikaKulkarni <117894317+RoopikaKulkarni@users.noreply.github.com>
2023-04-19 17:27:29 +0530

    Box effect class added in match market list, Betslip changed

--- src/app/shared/components/bet-slip/bet-slip.component.ts
+++ src/app/shared/components/bet-slip/bet-slip.component.ts
@@ -357,4 +357,13 @@ export class BetSlipComponent implements OnInit, OnChanges {
     this.betSlipParams['isBack'] = false;
   }
 
+  numberOnly(event): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+
+  }
+
 }
